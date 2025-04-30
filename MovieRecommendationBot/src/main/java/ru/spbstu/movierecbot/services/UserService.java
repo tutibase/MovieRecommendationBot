f75084f4f50b4671914641a28aa671e971831099
lizochka-bogdanovaa <lizabog2004@gmail.com>
2025-04-30 19:10:17 +0300

    feat: added UserService

---
+++ MovieRecommendationBot/src/main/java/ru/spbstu/movierecbot/services/UserService.java
@@ -0,0 +1,31 @@
+package ru.spbstu.movierecbot.services;
+import ru.spbstu.movierecbot.dbClasses.tables.records.UsersRecord;
+import ru.spbstu.movierecbot.dao.pg.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void registerUser(int telegramId) {
+        userDao.createUser(telegramId);
+    }
+
+    public Optional<UsersRecord> findUserByTelegramId(int telegramId) {
+        return userDao.getUserByTelegramId(telegramId);
+    }
+
+    public List<UsersRecord> getAllUsers(){
+        return userDao.getAllUsers();
+    }
+}
