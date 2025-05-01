ef78524878d31864660b00f545c4bb85ef9268da
tutibase <dykun1784@gmail.com>
2025-05-01 15:40:32 +0300

    refactor: ApiResponseDto is now an internal FilmDaoImpl class

--- MovieRecommendationBot/src/main/java/ru/spbstu/movierecbot/dto/ApiResponseDto.java
+++
@@ -1,7 +0,0 @@
-package ru.spbstu.movierecbot.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
-public record ApiResponseDto(@JsonProperty("docs") List<FilmDto> films) {}
