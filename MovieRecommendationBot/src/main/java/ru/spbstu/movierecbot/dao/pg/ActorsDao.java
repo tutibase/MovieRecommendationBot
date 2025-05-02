c5f08f8211f27d305b66e122fe457e0e9cd9fa5c
polyalugovenko <polinalugovenko0403@gmail.com>
2025-05-02 11:42:54 +0300

    fix: added setting actorId

--- MovieRecommendationBot/src/main/java/ru/spbstu/movierecbot/dao/pg/ActorsDao.java
+++ MovieRecommendationBot/src/main/java/ru/spbstu/movierecbot/dao/pg/ActorsDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface ActorsDao {
     //Добавление актера
-    int addActor(long telegramId, String fullName);
+    int addActor(long telegramId, int actorId, String fullName);
     //Получение списка актеров
     List<ActorsRecord> getActorsByTelegramId(long telegramId);
     //Удаление актера
