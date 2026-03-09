e5a04ce79d39c47dccd09e2fa56c241f2a32229d
polyalugovenko <polinalugovenko0403@gmail.com>
2026-03-09 15:12:56 +0300

    Jenkinsfile for build

---
+++ MovieRecommendationBot/build_job/build.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent {
+        label 'poly-agent'
+    }
+
+    options {
+        ansiColor('xterm')
+    }
+
+    environment {
+        DB_PASSWORD = credentials('POSTGRES_DB_PASSWORD')
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+                echo "Code checked out"
+            }
+        }
+
+        stage('Database Setup') {
+            steps {
+                // Используем права jenkins-poly (без sudo)
+                sh '''
+                    createdb users_db 2>/dev/null || echo "DB exists"
+                    psql -d users_db -f "$(pwd)/src/main/resources/users_db.sql"
+                '''
+            }
+        }
+
+        stage('Build & jOOQ') {
+            steps {
+                // Testcontainers сам поднимет докер-контейнер для генерации кода
+                sh "mvn clean package -DskipTests -Ddb.password=${DB_PASSWORD} -Dstyle.color=always"
+            }
+            post {
+                success {
+                    // Архивация артефакта (требование ЛР)
+                    archiveArtifacts artifacts: 'target/MovieRecommendationBot-*.jar', fingerprint: true
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            // Чистка за Testcontainers
+            sh "docker rm -f \$(docker ps -a -q --filter name=tc-) 2>/dev/null || true"
+            cleanWs()
+        }
+    }
+}
